8ccf95b4904c0a4e8269934c9499b8f5d63f78cb
rmalegr <rodrigomalegre@gmail.com>
2022-04-25 22:38:29 -0300

    cambiando build a docs

---
+++ docs/precache-manifest.6051f59679909914f3016759ad02e0ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1824263dea78028eb233c45cb00e7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fad98900783a107898e",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fad98900783a107898e",
+    "url": "/static/js/main.e6fbd1c7.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
